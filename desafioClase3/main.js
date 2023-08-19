23150b175b581ee984c396f25ff7c931b36f7239
NatalioStefanoMalagrino <malagrinonatalio@gmail.com>
2023-08-19 11:38:47 -0300

    Initial commit

---
+++ desafioClase3/main.js
@@ -0,0 +1,56 @@
+import Producto from "./classes/Producto.js";
+import ProductManager from "./classes/ProductManager.js";
+
+
+//MÉTODOS DE CREACIÓN DE PRODUCTOS//
+
+//////DESCOMENTAR Y EJECUTAR EL COMANDO "node main.js" EN CADA BLOQUE DEBIDO A QUE EL ASYNC NO PERMITE CREAR TODOS LOS PRODUCTOS EN UNA SOLA EJECUCIÓN//////
+
+//1.-  CREACIÓN DE PRODUCTOS //
+
+const producto1 = new Producto('Jamón', 3000, 'carnes', 'un estracto del cerdo', 'naranja', [], 'EEFFOL', 5)
+const manager1 = new ProductManager()
+manager1.addProduct(producto1)
+
+// const producto2 = new Producto('pan', 500, 'panadería', 'Un bien del pueblo', 'café', [], 'FFLLVF', 16)
+// const manager1 = new ProductManager()
+// manager1.addProduct(producto2)
+
+// const producto3 = new Producto('tomate', 1200, 'verduras', 'La verdura de Italia', 'rojo', [], 'GGBBLO', 11)
+// const manager1 = new ProductManager()
+// manager1.addProduct(producto3)
+
+// const producto4 = new Producto('manzana', 800, 'fruta','fruto del Eden', 'roja', [], 'YYNVBT', 22)
+// const manager1 = new ProductManager()
+// manager1.addProduct(producto4)
+
+
+/* ---------------------------------------------------------------------------------------------------- */
+
+//SE SUGIERE COMENTAR TODOS LOS "MÉTODOS DE CREACIÓN DE PRODUCTOS" ANTES DE EJECUTAR LOS SIGUIENTES MÉTODOS//
+
+
+//EJECUTAR CADA PUNTO POR SEPARADO DESCOMENTÁNDOLO SEGÚN SE SOLICITE REVISAR//
+
+//2.- Obtener Todos los Productos
+// const manager1 = new ProductManager() //Crea la instancia para la ejecución del método
+// manager1.getProductos()  // Retrorna todos los productos creados
+
+//3.- Obtener 1 producto por su ID
+// const manager1 = new ProductManager() //Crea la instancia para la ejecución del método
+// manager1.getProductById(2) // Retorna el Producto con ID 2
+// manager1.getProductById(7) // Producto No encontrado
+
+//4.- Eliminar producto por ID
+// const manager1 = new ProductManager() //Crea la instancia para la ejecución del método
+// manager1.deleteProductById(1) // El Producto con ID x ha sido Eliminado
+
+//5.- Elimina Todos los productos
+// const manager1 = new ProductManager() //Crea la instancia para la ejecución del método
+// manager1.deleteAllProducts() //Todos los productos Eliminados
+
+//6.- Actualizar NOMBRE de producto según el ID
+// const manager1 = new ProductManager() //Crea la instancia para la ejecución del método
+//El siguiente producto es de ejemplo
+// const producto4 = new Producto('manzana', 800, 'fruta','fruto del Eden', 'roja', [], 'YYNVBT', 22)
+// manager1.UpdateProductById(2, producto4) // Elija un nuevo nombre para el producto: // Nombre de producto actualzado
